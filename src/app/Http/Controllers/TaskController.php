57048f02df12079ba74c7b79810b0db5181938a6
aarobc <aaron.b.christiansen@gmail.com>
2016-08-22 17:25:10 -0600

    backend done, now to angular

---
+++ src/app/Http/Controllers/TaskController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tasks = Task::all();
+        return $tasks;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $task = new Task;
+        $dat = $request->all();
+
+        $device->fill($task);
+        return $device;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($task)
+    {
+        return $task;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $task)
+    {
+        //
+        $this->validate($request, [
+            'task' => 'min:1'
+        ]);
+
+        $dat = $request->all();
+        $task=>fill($dat);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($task)
+    {
+        $task->delete();
+    }
+}
---
+++ src/resources/views/tasks.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta id="token" name="token" value="{{ csrf_token() }}">
+        <title>Demo Form</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
+
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+    </body>
+</html>
